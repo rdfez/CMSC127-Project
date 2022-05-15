bd95b4651ba9afecad987629890c0785625969e8
rdfez <95547752+rdfez@users.noreply.github.com>
2022-05-16 07:25:09 +0800

    Changed data type of priority and status

--- task_app.sql
+++ task_app.sql
@@ -1,7 +1,7 @@
 CREATE TABLE category (
     categoryid INT(2) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    priority INT(2) NOT NULL,
+    cname VARCHAR(50) NOT NULL,
+    priority VARCHAR(6) NOT NULL,
     color VARCHAR(20),
     CONSTRAINT category_categoryid_pk PRIMARY KEY (categoryid)
 );
@@ -10,9 +10,9 @@ CREATE TABLE task (
 	taskid INT(2) NOT NULL,
 	title VARCHAR(50) NOT NULL,
 	details VARCHAR(500) NOT NULL,
-	status BOOLEAN NOT NULL DEFAULT 0,
+	status VARCHAR(11) NOT NULL DEFAULT "In Progress",
 	duedate DATE,
     categoryid INT(2),
-	CONSTRAINT task_taskid_pk PRIMARY KEY(taskid),
-    CONSTRAINT task_categoryid_fk FOREIGN KEY(categoryid) REFERENCES category(categoryid)
+	CONSTRAINT task_taskid_pk PRIMARY KEY (taskid),
+    CONSTRAINT task_categoryid_fk FOREIGN KEY (categoryid) REFERENCES category (categoryid)
 );

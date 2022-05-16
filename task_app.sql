5b4151244d7aed44a1339136a0a16fb8e7da294a
rdfez <95547752+rdfez@users.noreply.github.com>
2022-05-16 12:37:01 +0800

    Update task_app.sql

--- task_app.sql
+++ task_app.sql
@@ -7,12 +7,51 @@ CREATE TABLE category (
 );
 
 CREATE TABLE task (
-	taskid INT(2) NOT NULL,
-	title VARCHAR(50) NOT NULL,
-	details VARCHAR(500) NOT NULL,
-	status VARCHAR(11) NOT NULL DEFAULT "In Progress",
-	duedate DATE,
+    taskid INT(2) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    details VARCHAR(500) NOT NULL,
+    status VARCHAR(11) NOT NULL DEFAULT "new",
+    duedate DATE,
     categoryid INT(2),
-	CONSTRAINT task_taskid_pk PRIMARY KEY (taskid),
+    CONSTRAINT task_taskid_pk PRIMARY KEY (taskid),
     CONSTRAINT task_categoryid_fk FOREIGN KEY (categoryid) REFERENCES category (categoryid)
 );
+
+--Add category
+INSERT INTO category VALUES (1, "Acad-related", "Urgent", "Pink"), (2, "Org-related", "High", "Violet"), (3, "Movies to watch", "Low", "Orange"), (4, "Personal", "Normal", "Yellow");
+
+--Add/Create Task
+INSERT INTO task VALUES (01, "CMSC 127 Exercise 1", "ER Model/EER Model", "new", "2022-05-30", 1), (02, "CMSC 131 Exercise 1", "Basic Arithmetics", "new", "2022-05-22", NULL), (03, "CMSC 131 Quiz 1", "Basic Arithmetics", "new", "2022-05-22", 1);
+
+--Edit Task
+UPDATE task SET status = "in progress" WHERE taskid = 01;
+
+--Delete Task
+DELETE FROM task WHERE title = "CMSC 127 Exercise 1";
+
+--View Task
+SELECT taskid, title, details, status, duedate FROM task WHERE status = "in progress";
+
+--View All Tasks 
+SELECT taskid, title, details, status, duedate FROM task;
+
+--Mark Task as Done
+UPDATE task SET status = "done" WHERE title = "CMSC 131 Quiz 1";
+
+--Edit category
+UPDATE category SET cname="Acads" WHERE cname="Acad-related";
+
+--Delete category
+DELETE FROM category WHERE categoryid IN (2, 3);
+
+--View category
+SELECT * FROM category WHERE cname="Personal";
+
+--Add a task to category
+UPDATE task SET categoryid=1 WHERE title="CMSC 131 Exercise 1";
+
+--View task per day
+SELECT * FROM task WHERE duedate = "2022-05-22";
+
+--View task per month
+SELECT * FROM task WHERE MONTH(duedate) = 5;
